9a75d6c85608d6ab1c03df127ceb41d31767bf8c
EC2 Default User <ec2-user@ip-172-30-1-224.ap-northeast-1.compute.internal>
2019-12-13 08:54:29 +0000

    path loop

--- _simple.sh.c
+++ _simple.sh.c
@@ -1,12 +1,41 @@
 #include <stdio.h>
+#include <sys/stat.h>
+#include <string.h>
+
+const char const * path[] = {
+    "/usr/bin/",
+    "/bin/"
+};
+
+int isPathExist(const char *path){
+    if( stat ( path , NULL ) == 0 ){
+        return -1;
+    }else{
+	return 0;
+    }
+}
+
+#define BUFFERSIZE 4096
+char buffer[BUFFERSIZE];
+
+
+
 
 int main (int argc,char *argv[])
 {
     int ret = 0;
 
-    for(int i=0;i<argc;++i){
-        printf("i=%d:%s\n",i,argv[i]);
+    for(int i=0;i<2;i++){
+        strcpy(buffer,path[i]);
+	int len = strlen(buffer);
+	strcpy(buffer+len,argv[1]);
+	printf("%s\n",buffer);
     }
+
+
+//for(int i=0;i<argc;++i){
+//        printf("i=%d:%s\n",i,argv[i]);
+//    }
 
     return ret;
 }
